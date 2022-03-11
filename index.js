cee23e991f9f73ec6d510f82be307fd16411196f
andres beyra <andresbeyra@gmail.com>
2022-03-11 00:23:48 -0500

    set up project and began building out routes for all videos, dynamic routing for clicking video in next videos list, and post route

---
+++ index.js
@@ -0,0 +1,24 @@
+const uuid = require('uuid');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const videosRoutes = require('./routes/videos');
+
+const app = express();
+
+app.use(express.static('./public'));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/videos', videosRoutes);
+
+const SERVER_PORT = process.env.PORT || 9000;
+
+// Start up the app
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is listening on port ${SERVER_PORT}`);
+});
+
