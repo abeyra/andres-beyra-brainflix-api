c4dd648fc0a9dbd1faf37fff335e8246494c29cb
andres beyra <andresbeyra@gmail.com>
2022-03-11 21:59:41 -0500

    got rid of icons and logo folders within public because i didn't need them. was able to update code within videos route file to successfully hardcode image into newvideo object that is posted from upload page

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const uuid = require('uuid');
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const videosRoutes = require('./routes/videos');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const videosRoutes = require("./routes/videos");
 
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use('/videos', videosRoutes);
+app.use("/videos", videosRoutes);
 
 const SERVER_PORT = process.env.PORT || 9000;
 
@@ -21,4 +19,3 @@ const SERVER_PORT = process.env.PORT || 9000;
 app.listen(SERVER_PORT, () => {
   console.log(`Server is listening on port ${SERVER_PORT}`);
 });
-
--- routes/videos.js
+++ routes/videos.js
@@ -1,91 +1,85 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
-// const {Bike} = require('../public/images/Upload-video-preview.jpg');
+const fs = require("fs");
 
 function readVideos() {
-
-  const videosData = fs.readFileSync('./data/video-details.json');
-
+  const videosData = fs.readFileSync("./data/video-details.json");
   const parsedVideos = JSON.parse(videosData);
   return parsedVideos;
 }
 
 function writeVideos(data) {
-
   const stringifiedData = JSON.stringify(data);
-  fs.writeFileSync('./data/video-details.json', stringifiedData);
+  fs.writeFileSync("./data/video-details.json", stringifiedData);
 }
 
 //all videos
-router.get('/', (request, response) => {
-    const parsedData = readVideos();
-    response.json(parsedData);
-    console.log('working');
-})
+router.get("/", (request, response) => {
+  const parsedData = readVideos();
+  response.json(parsedData);
+  console.log("working");
+});
 
-//single video 
-router.get('/:id', (request, response) => {
-    console.log('getting single video');
-    const parsedData = readVideos();
+//single video
+router.get("/:id", (request, response) => {
+  console.log("getting single video");
+  const parsedData = readVideos();
+  const individualVideo = parsedData.find(
+    (video) => video.id === request.params.id
+  );
 
-    const individualVideo = parsedData.find((video) => video.id === request.params.id)
+  if (!individualVideo) {
+    return response.status(404).send("video not found!");
+  }
+  response.json(individualVideo);
+});
 
-    if(!individualVideo){
-      return response.status(404).send('video not found!');
-    }
-
-    response.json(individualVideo);
-})
-    
 //post upload
-router.post('/', (request, response) => {
-    const videos = readVideos();
-
-    // const staticImage = {Bike}; 
-
-    const {title, description, id} = request.body;
-    console.log(request.body);
-
-    const newVideo = {
-      title,
-      description,
-      id,
-      image: Bike,
-      channel: 'Bitcoin',
-      views: '0',
-      likes: '0',
-      duration: '1:00',
-      timestamp: new Date().toLocaleDateString(),
-      comments: [
+router.post("/", (request, response) => {
+  const videos = readVideos();
+  const { title, description, id} = request.body;
+  console.log(request.body);
+  const newVideo = {
+    title,
+    description,
+    id,
+    image: "http://localhost:5001/images/Upload-video-preview.jpg",
+    channel: "Bitcoin",
+    views: "0",
+    likes: "0",
+    duration: "1:00",
+    timestamp: new Date().toLocaleDateString(),
+    comments: [
       {
-        "name": "Micheal Lyons",
-        "comment": "They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.",
-        "likes": 0,
-        "timestamp": 1628522461000
+        name: "Micheal Lyons",
+        comment:
+          "They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.",
+        likes: 0,
+        timestamp: 1628522461000,
       },
       {
-        "name": "Gary Wong",
-        "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
-        "likes": 0,
-        "timestamp": 1626359541000
+        name: "Gary Wong",
+        comment:
+          "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+        likes: 0,
+        timestamp: 1626359541000,
       },
       {
-        "name": "Theodore Duncan",
-        "comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Every time I see him I feel instantly happy! He is definitely my favorite ever!",
-        "likes": 0,
-        "timestamp": 1626011132000
-      }
-    ]
-    }
+        name: "Theodore Duncan",
+        comment:
+          "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Every time I see him I feel instantly happy! He is definitely my favorite ever!",
+        likes: 0,
+        timestamp: 1626011132000,
+      },
+    ],
+  };
 
-    videos.push(newVideo);
-    writeVideos(videos);
+  videos.push(newVideo);
+  writeVideos(videos);
 
-    console.log(videos);
+  console.log(videos);
 
-    response.status(201).json(newVideo);
-})
+  response.status(201).json(newVideo);
+});
 
 module.exports = router;
