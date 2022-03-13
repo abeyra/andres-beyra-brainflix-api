e188d4e246b219b8f7dd8e762f08ce3a13c1eb47
andres beyra <andresbeyra@gmail.com>
2022-03-13 19:59:44 -0400

    changed port number and added all video images for next videos list. updated code in videos route to pass an empty array for comments instead of 3 default comments. added id to comments for videos list

--- routes/videos.js
+++ routes/videos.js
@@ -16,7 +16,16 @@ function writeVideos(data) {
 //all videos
 router.get("/", (request, response) => {
   const parsedData = readVideos();
-  response.json(parsedData);
+
+  const mappedData = parsedData.map((video) => {
+    return {
+      id: video.id,
+      title: video.title,
+      image: video.image,
+      channel: video.channel
+    }
+  })
+  response.json(mappedData);
 });
 
 //single video
@@ -40,35 +49,13 @@ router.post("/", (request, response) => {
     title,
     description,
     id,
-    image: "http://localhost:5001/images/Upload-video-preview.jpg",
+    image: "http://localhost:8080/images/Upload-video-preview.jpg",
     channel: "Bitcoin",
     views: "0",
     likes: "0",
     duration: "1:00",
-    timestamp: new Date().toLocaleDateString(),
-    comments: [
-      {
-        name: "Micheal Lyons",
-        comment:
-          "They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.",
-        likes: 0,
-        timestamp: 1628522461000,
-      },
-      {
-        name: "Gary Wong",
-        comment:
-          "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
-        likes: 0,
-        timestamp: 1626359541000,
-      },
-      {
-        name: "Theodore Duncan",
-        comment:
-          "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Every time I see him I feel instantly happy! He is definitely my favorite ever!",
-        likes: 0,
-        timestamp: 1626011132000,
-      },
-    ],
+    timestamp: new Date().getTime(),
+    comments: []
   };
 
   videos.push(newVideo);
